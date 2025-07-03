06395afe2c00ab48f5a9ca7ac59102a52dfed9be
Ilya-Kanstanchuk <ilyakonst95@gmail.com>
2025-07-04 00:12:14 +0200

    init smart-menu-server

---
+++ smart-menu-server/src/main/java/com/ilyaproject/smart_menu_server/SmartMenuServerApplication.java
@@ -0,0 +1,13 @@
+package com.ilyaproject.smart_menu_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMenuServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMenuServerApplication.class, args);
+	}
+
+}
---
+++ smart-menu-server/src/test/java/com/ilyaproject/smart_menu_server/SmartMenuServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilyaproject.smart_menu_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMenuServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
