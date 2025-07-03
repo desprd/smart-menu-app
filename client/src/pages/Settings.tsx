baa1a6d7488d7616a80f2c79ea2b00082b846491
Ilya-Kanstanchuk <ilyakonst95@gmail.com>
2025-07-03 13:26:23 +0200

    Add client: error handling for Settings page

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -4,10 +4,20 @@ import SubmitButton from "../components/SubmitButton";
 import FormInput from "../components/FormInput";
 
 const Settings: React.FC = () => {
-  const handleSubmit = () => {};
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [errorMessage, setErrorMessage] = useState("");
+  const handleSubmit = () => {
+    if (password.length < 6) {
+      setErrorMessage("Password supposed to be at least 6 characters long");
+      return;
+    }
+    if (!email.includes("@") || !email.includes(".")) {
+      setErrorMessage("Please enter a valid email address");
+      return;
+    }
+    console.log("Changed:", { email, password });
+  };
   return (
     <div className="relative flex min-h-screen flex-col bg-white group/design-root overflow-x-hidden font-lexend">
       <div className="layout-container flex h-full grow flex-col">
@@ -24,6 +34,11 @@ const Settings: React.FC = () => {
             <h3 className="text-[#111714] text-lg font-bold tracking-[-0.015em] px-4 pb-2 pt-4">
               Change credentials
             </h3>
+            {errorMessage && (
+              <div className="text-red-600 text-sm font-medium px-2 md:px-0 pt-2">
+                {errorMessage}
+              </div>
+            )}
             <FormInput
               value={email}
               onChange={setEmail}
