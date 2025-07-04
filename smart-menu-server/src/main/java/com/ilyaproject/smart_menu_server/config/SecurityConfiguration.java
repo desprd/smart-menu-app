97ff696db04d50f9e64dd321ca1df93086a9f848
Ilya-Kanstanchuk <ilyakonst95@gmail.com>
2025-07-04 12:29:35 +0200

    Add server: ApplicationConfiguration and CustomUserService classes

---
+++ smart-menu-server/src/main/java/com/ilyaproject/smart_menu_server/config/SecurityConfiguration.java
@@ -0,0 +1,12 @@
+package com.ilyaproject.smart_menu_server.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration  {
+
+}
