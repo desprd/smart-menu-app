e7de08f624cf64fc045f750eb2f0e47ab781345d
Ilya-Kanstanchuk <ilyakonst95@gmail.com>
2025-07-13 12:27:37 +0200

    Fix server: change email sending process from gmail smtp to mailgun

--- smart-menu-server/src/main/java/com/ilyaproject/smart_menu_server/email/generator/RecipesInPdfMessageGenerator.java
+++ smart-menu-server/src/main/java/com/ilyaproject/smart_menu_server/email/generator/RecipesInPdfMessageGenerator.java
@@ -40,8 +40,8 @@ public class RecipesInPdfMessageGenerator {
             Properties props = new Properties();
             props.put("mail.smtp.auth", "true");
             props.put("mail.smtp.starttls.enable", "true");
-            props.put("mail.smtp.host", "smtp.gmail.com");
-            props.put("mail.smtp.port", "587");
+            props.put("mail.smtp.host", "smtp.eu.mailgun.org");
+            props.put("mail.smtp.port", "2525");
             Session session = Session.getInstance(props, new Authenticator() {
                 @Override
                 protected PasswordAuthentication getPasswordAuthentication() {
